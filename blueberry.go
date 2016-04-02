bd4379998a5e967744b6dd0f29a1377ce7cc177b
Literate Snow <literatesnow@users.noreply.github.com>
2016-04-02 13:42:55 +1300

    Add HTTP server and very basic API - #3

--- blueberry.go
+++ blueberry.go
@@ -1,35 +1,58 @@
 package main
 
 import (
-	"fmt"
-
 	"bitbucket.org/unrulyknight/scgi"
 	"bitbucket.org/unrulyknight/xmlrpc"
+	"github.com/julienschmidt/httprouter"
+
+	"fmt"
+	"log"
+	"net/http"
 )
 
+type rtorrent struct {
+	scgi *scgi.Client
+}
+
+var rt rtorrent
+
 func main() {
-	cl := scgi.NewClient("tcp", "192.168.1.15:50000")
+	rt = rtorrent{scgi: scgi.NewClient("tcp", "192.168.1.15:50000")}
 
-	//system.client_version, download_list
+	router := httprouter.New()
+	router.GET("/rtorrent/list", rtlist)
+	router.GET("/rtorrent/version", rtversion)
+	router.ServeFiles("/public/*filepath", http.Dir("./www/public"))
 
-	doc := xmlrpc.CreateRequest("d.multicall", []xmlrpc.Value{
+	log.Fatal(http.ListenAndServe("localhost:8080", router))
+}
+
+func rtlist(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	rtrequest(w, "d.multicall", []xmlrpc.Value{
 		xmlrpc.NewString("main"),
 		xmlrpc.NewString("d.base_filename="),
 		xmlrpc.NewString("d.base_path="),
 		xmlrpc.NewString("d.bytes_done="),
 		xmlrpc.NewString("d.is_private=")})
+}
 
-	response, err := cl.Request(doc)
+func rtversion(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	rtrequest(w, "system.client_version", nil)
+}
+
+func rtrequest(w http.ResponseWriter, methodName string, params []xmlrpc.Value) {
+	w.Header().Set("Content-Type", "text/plain")
+
+	doc := xmlrpc.CreateRequest(methodName, params)
+	response, err := rt.scgi.Request(doc)
 
 	if err != nil {
-		fmt.Printf("%s\n", err)
+		fmt.Fprintf(w, "Error: %s\n", err)
 		return
 	}
 
-	fmt.Printf("%s\n", string(response.Bytes()))
-
 	values := xmlrpc.ParseResponse(response)
 	for _, val := range values {
-		fmt.Printf("%s\n", val.Print())
+		fmt.Fprintf(w, "%s\n", val.Print())
 	}
 }
